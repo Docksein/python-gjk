4050d6015e8bb45b4061011a7c8cc4eb3dc794fb
Docksein <58305245+Docksein@users.noreply.github.com>
2019-12-04 18:26:22 +0100

    Add files via upload

--- e.py
+++ e.py
@@ -1,17 +1,14 @@
 import requests
-import json
-
 
 def ship_detail(ShipName):
-    x = requests.get("https://swapi.co/api/starships/?search=" + ShipName)
-    data = x.json()["results"][0]
-    return {data["name"], data["model"], data["starship_class"], data["hyperdrive_rating"]}
+    x = requests.get("https://swapi.co/api/starships/?search=" + ShipName).json()["results"][0]
+    return {x["name"], x["model"], x["starship_class"], x["hyperdrive_rating"]}
 
 
 def species_detail(SpeciesName):
-    x = requests.get("https://swapi.co/api/species/?search=" + SpeciesName)
-    data = x.json()["results"][0]
-    return {data["name"], data["classification"], data["designation"]}
+    x = requests.get("https://swapi.co/api/species/?search=" + SpeciesName).json()["results"][0]
+    y = requests.get(x["homeworld"]).json()
+    return {x["name"], x["classification"], x["designation"], y["name"]}
 
 
 def person_detail(Name):
@@ -21,9 +18,11 @@ def person_detail(Name):
 
 
 def film_detail(filmname):
-    x = requests.get("https://swapi.co/api/films/" + filmname)
-    data = x.json()["results"][0]
-    return {data["title"], data["director"], data["episode_id"]}
+    x = requests.get("https://swapi.co/api/films/" + filmname).json()["results"][0]
+    return {x["title"], x["director"], x["episode_id"]}
 
 
-print(person_detail("luke"))
+try:
+    print(species_detail("ewok"))
+except IndexError as err:
+    print(err)
