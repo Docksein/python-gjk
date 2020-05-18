61f6ede327debfcb5de287bcf2f898ae22f60b59
Docksein <58305245+Docksein@users.noreply.github.com>
2020-05-18 17:55:12 +0200

    Add files via upload

--- piskvorky.py
+++ piskvorky.py
@@ -8,23 +8,62 @@ def board():
 	print(f"{game[2][0]}|{game[2][1]}|{game[2][2]}")
 
 def player_input():
+	global x
+	global y
 	x = input("select x axis: ")
 	y =input("select y axis: ")
-	while 4 < int(x) > 0 or  4 < int(y) > 0 :
-		print("not in range")
-		x = input("select x axis: ")
-		y =input("select y axis: ")
-	else:
+	independent_variable = 0
+	while independent_variable < 1:
 		try:
 			x = int(x) - 1
 			y = int(y)- 1
+			if	int(x) < 0 or int(x) > 3 or int(y) < 0 or int(y) > 3: 
+				print("Not in range")
+				x = input("select x axis: ")
+				y =input("select y axis: ")
+			else:
+				independent_variable += 1
 		except ValueError as errv:
 			print(errv)
+			x = input("select x axis: ")
+			y =input("select y axis: ")
 		except TypeError as errt:
 			print(errt)
+			x = input("select x axis: ")
+			y =input("select y axis: ")
+		
+def win_check():
+	for i in range(3):
+		if game[i][0] == game[i][1] == game[i][2] == x_or_o or game[0][i] == game[1][i] == game[2][i] == x_or_o: 
+			board()
+			print(f"Congratulations, {x_or_o} won!")
+			return True
+		elif  game[0][0] == game[1][1] == game[2][2] == x_or_o or  game[0][2] == game[1][1] == game[2][0] == x_or_o:   
+			board()
+			print(f"Congratulations, {x_or_o} won!")
+			return True
+		else:
+			return False
+		
+def board_change():
+	while game[y][x]=="x" or game[y][x]=="y":
+		print("That spot has already been taken")
+		player_input()
+	else:
+		game[y][x] = x_or_o 
 
 def main():
-	board()
-	player_input()
+	for i in range(9):
+		global x_or_o
+		x_or_o = " "
+		if i%2 == 0:
+			x_or_o = "x"
+		else:
+			x_or_o = "o"
+		board()
+		player_input()
+		board_change()
+		if win_check():
+			break
 
 main()
